9a988d9b92954d43cd9843ea54b303ff1eae8873
zengchen1024 <chenzeng2@huawei.com>
2023-05-08 20:12:39 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -7,3 +7,9 @@ cfg=$1
 ./generate-mirrorlist-cache -c $cfg -o ./config/mirrorlist_cache.proto
 
 ./mirrorlist-server -c ./config/mirrorlist_cache.proto -g ./config/global_netblocks.txt --log ./mirrorlist-server.log --cccsv ./config/country_continent.csv --geoip ./config/GeoLite2-Country.mmdb
+
+# even the service above is killed manually, then we can start it again but will not kill the container
+while true
+do
+    sleep 1
+done
