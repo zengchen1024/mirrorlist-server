0442d524d04f7d2f07fe106551baf618c3a064ac
zengchen1024 <chenzeng2@huawei.com>
2023-05-08 20:19:45 +0800

    fix listen

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ cfg=$1
 
 ./generate-mirrorlist-cache -c $cfg -o ./config/mirrorlist_cache.proto
 
-./mirrorlist-server -c ./config/mirrorlist_cache.proto -g ./config/global_netblocks.txt --log ./mirrorlist-server.log --cccsv ./config/country_continent.csv --geoip ./config/GeoLite2-Country.mmdb
+./mirrorlist-server --listen 0.0.0.0 -c ./config/mirrorlist_cache.proto -g ./config/global_netblocks.txt --log ./mirrorlist-server.log --cccsv ./config/country_continent.csv --geoip ./config/GeoLite2-Country.mmdb
 
 # even the service above is killed manually, then we can start it again but will not kill the container
 while true
